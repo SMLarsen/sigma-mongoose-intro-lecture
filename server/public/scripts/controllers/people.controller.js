2532a55822e2507bfb756a75be55b3e2fe9f8631
Steve Larsen <stevelarsen01@gmail.com>
2016-12-01 17:16:17 -0600

    styled by niceness

--- server/public/scripts/controllers/people.controller.js
+++ server/public/scripts/controllers/people.controller.js
@@ -2,7 +2,7 @@ myApp.controller('PeopleController', ['$http', function($http) {
     var self = this;
     self.people = [];
     self.newPerson = {};
-    self.niceLevels = [1,2,3,4,5];
+    self.niceLevels = ['Angelic', 'Great', 'NotTooBad', 'Weak', 'DamnedToHell'];
 
     // Start app
     getData();
